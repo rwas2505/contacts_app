d82cda3a1a6492a3d40e92a6e61f49e2930701d3
ryan <ryanwaszak@gmail.com>
2020-03-10 19:31:49 -0500

    add middle_name and bio attributes via new migration files

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -23,9 +23,11 @@ class Api::ContactsController < ApplicationController
     @contact = Contact.find_by(id: params[:id])
     @contact.update(
       first_name: params[:first_name] || @contact.first_name,
+      middle_name: params[:middle_name] || @contact.middle_name,
       last_name: params[:last_name] || @contact.last_name,
       phone_number: params[:phone_number] || @contact.phone_number,
       email: params[:email] || @contact.email,
+      bio: params[:bio] || @contact.bio
     )
     render "show.json.jb"
   end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,6 +4,6 @@ class Contact < ApplicationRecord
   end
 
   def full_name
-    "#{first_name} #{last_name}"
+    "#{first_name} #{middle_name} #{last_name}"
   end
 end
